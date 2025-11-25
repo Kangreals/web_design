a26a9961b92ea3a7e9472ede31891e82d2c97375
Syahrial <syahrialbunder@gmail.com>
2025-11-25 15:48:59 +0700

    update

--- app.py
+++ app.py
@@ -6,11 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form.get('name')
